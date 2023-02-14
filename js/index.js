3b1452787eb085c82171de3849a14187e38b8936
Ayush <94029454+ayush-Web-dev@users.noreply.github.com>
2023-02-14 19:33:21 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,96 @@
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.padding = "8px 10px";
+    document.getElementById("logo").style.fontSize = "34px";
+  } else {
+    document.getElementById("navbar").style.padding = "18px 10px";
+    document.getElementById("logo").style.fontSize = "40px";
+  }
+}
+
+const form = document.getElementById('contactForm');
+form.addEventListener("change",() => {
+    document.getElementById('sendMessageButton').disabled = !form.checkValidity()
+});
+
+
+const section = document.querySelector("section");
+const btnKnow = section.querySelector(".btnKnow");
+const btnClose = section.querySelector(".btnClose");
+
+function openBody(){
+    section.classList.add("open");
+}
+
+function closeBody(){
+    section.classList.remove("open");
+}
+
+btnKnow.addEventListener("click", openBody);
+
+btnClose.addEventListener("click", closeBody);
+
+
+var basicTimeline = anime.timeline({
+  autoplay: false
+});
+
+var pathEls = $(".check");
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute("stroke-dashoffset", offset);
+}
+
+basicTimeline
+  .add({
+    targets: ".text",
+    duration: 1,
+    opacity: "0"
+  })
+  .add({
+    targets: ".button",
+    duration: 1300,
+    height: 10,
+    width: 300,
+    backgroundColor: "#2B2D2F",
+    border: "0",
+    borderRadius: 100
+  })
+  .add({
+    targets: ".progress-bar",
+    duration: 2000,
+    width: 300,
+    easing: "linear"
+  })
+  .add({
+    targets: ".button",
+    width: 0,
+    duration: 1
+  })
+  .add({
+    targets: ".progress-bar",
+    width: 80,
+    height: 80,
+    delay: 500,
+    duration: 750,
+    borderRadius: 80,
+    backgroundColor: "#71DFBE"
+  })
+  .add({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: 200,
+    easing: "easeInOutSine"
+  });
+
+$(".button").click(function() {
+  basicTimeline.play();
+});
+
+$(".text").click(function() {
+  basicTimeline.play();
+});
+
+
+
