ba54ca2e03c3066fc34e264cd0ff43261c08ee38
Ayush <94029454+ayush-Web-dev@users.noreply.github.com>
2023-04-24 16:53:38 +0530

    Portfolio

--- js/index.js
+++ js/index.js
@@ -31,29 +31,66 @@ btnKnow.addEventListener("click", openBody);
 btnClose.addEventListener("click", closeBody);
 
 
-const text=document.querySelector(".fancy");
-const strtext=text.textContent;
-console.log(strtext)
-const splitText=strtext.split("");
-text.textContent="";
-for(let i=0;i<splitText.length;i++)
-{
-    text.innerHTML+="<tag>"+splitText[i]+"</tag>";
+var basicTimeline = anime.timeline({
+  autoplay: false
+});
+
+var pathEls = $(".check");
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute("stroke-dashoffset", offset);
 }
-let char=0;
-let timer=setInterval(onTick,50);
-function onTick(){
-    const span=text.querySelectorAll('tag')[char];
-    span.classList.add('show');
-    char++;
-    if(char===splitText.length)
-    {
-        complete();
-        return;
-    }
-    function complete()
-    {
-        clearInterval(timer);
-        timer=null;
-    }
-}
+
+basicTimeline
+  .add({
+    targets: ".text",
+    duration: 1,
+    opacity: "0"
+  })
+  .add({
+    targets: ".button",
+    duration: 1300,
+    height: 10,
+    width: 300,
+    backgroundColor: "#2B2D2F",
+    border: "0",
+    borderRadius: 100
+  })
+  .add({
+    targets: ".progress-bar",
+    duration: 2000,
+    width: 300,
+    easing: "linear"
+  })
+  .add({
+    targets: ".button",
+    width: 0,
+    duration: 1
+  })
+  .add({
+    targets: ".progress-bar",
+    width: 80,
+    height: 80,
+    delay: 500,
+    duration: 750,
+    borderRadius: 80,
+    backgroundColor: "#71DFBE"
+  })
+  .add({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: 200,
+    easing: "easeInOutSine"
+  });
+
+$(".button").click(function() {
+  basicTimeline.play();
+});
+
+$(".text").click(function() {
+  basicTimeline.play();
+});
+
+
+
