84fbe6f1522086a4a9a3bb97626fd556e5b0b40e
Ayush <ayush1906031018@gmail.com>
2023-10-24 23:10:46 +0530

    Modify Portfolio

--- js/index.js
+++ js/index.js
@@ -8,10 +8,68 @@ function scrollFunction() {
   }
 }
 
-const form = document.getElementById('contactForm');
-form.addEventListener("change",() => {
-    document.getElementById('sendMessageButton').disabled = !form.checkValidity()
-});
+
+const forms = document.querySelectorAll(".needs-validation");
+const result = document.getElementById("result");
+
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+    "submit",
+    function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+            form.querySelectorAll(":invalid")[0].focus();
+        } else {
+            const formData = new FormData(form);
+            event.preventDefault();
+            event.stopPropagation();
+            const object = {};
+            formData.forEach((value, key) => {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+            result.innerHTML = "Please wait...";
+
+            fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+                },
+                body: json
+            })
+            .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+                result.classList.remove("text-secondary");
+                result.classList.add("text-success");
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+                result.classList.remove("text-secondary");
+                result.classList.add("text-danger");
+            }
+            })
+            .catch((error) => {
+                console.log(error);
+                result.innerHTML = "Something went wrong!";
+            })
+            .then(function () {
+                form.reset();
+                form.classList.remove("was-validated");
+                setTimeout(() => {
+                    result.style.display = "none";
+                }, 5000);
+            });
+        }
+        form.classList.add("was-validated");
+    },
+    false
+    );
+}); 
 
 
 const section = document.querySelector("section");
@@ -57,3 +115,45 @@ function onTick(){
         timer=null;
     }
 }
+
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain attribute:*/
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+        /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            /*remove the attribute, and call this function once more:*/
+            elmnt.removeAttribute("include-html");
+            includeHTML();
+            }
+        }      
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /*exit the function:*/
+        return;
+        }
+    }
+}; 
+
+// JavaScript code
+document.querySelector('.footer-link[href="#"]').addEventListener('click', function(event) {
+    event.preventDefault();
+    var resumeModal = new bootstrap.Modal(document.getElementById('resumeModal'));
+    resumeModal.show();
+});
+
+document.querySelector('.resume-link[href="#"]').addEventListener('click', function(event) {
+    event.preventDefault();
+    var resumeModal = new bootstrap.Modal(document.getElementById('resumeModal'));
+    resumeModal.show();
+});
